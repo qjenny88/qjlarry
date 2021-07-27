a4622ebf1c5eb2af2871d577d4d718902cfc9ece
qjenny88 <87863929+qjenny88@users.noreply.github.com>
2021-07-28 01:11:39 +0200

    Add files via upload

---
+++ alert.py
@@ -0,0 +1,66 @@
+import ccxt, yfinance
+import pandas_ta as ta
+import pandas as pd
+import requests
+
+exchange = ccxt.binance()
+
+bars = exchange.fetch_ohlcv('ETH/USDT', timeframe='5m', limit=500)
+df = pd.DataFrame(bars, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
+
+adx = df.ta.adx()
+macd = df.ta.macd(fast=14, slow=28)
+rsi = df.ta.rsi()
+
+df = pd.concat([df, adx, macd, rsi], axis=1)
+
+print(df)
+
+
+last_row = df.iloc[-1]
+
+print(last_row)
+
+WEBHOOK_URL = "https://discord.com/api/webhooks/869678756992790570/VZMEfkeZxX0OBnaRC6CiIxhprivRiUepuhGeG0EyPoa2QqiVClhTvZ3Nc6_3aZ5QZUam"
+
+if last_row['ADX_14'] >= 25:
+    if last_row['DMP_14'] > last_row['DMN_14']:
+        message = f"STRONG UPTREND: The ADX is {last_row['ADX_14']:.2f}"
+    if last_row['DMN_14'] > last_row['DMP_14']:
+        message = f"STRONG DOWNTREND: The ADX is {last_row['ADX_14']:.2f}"
+
+    payload = {
+        "username": "alertbot",
+        "content": message,
+    }
+
+    requests.post(WEBHOOK_URL, json=payload)
+
+if last_row['ADX_14'] < 25:
+    message = f"NO TREND: The ADX is {last_row['ADX_14']:.2f}"
+
+    payload = {
+        "username": "alertbot",
+        "content": message
+    }
+
+    requests.post(WEBHOOK_URL, json=payload)
+
+# print(adx)
+# ticker = yfinance.Ticker("TSLA")
+# df = ticker.history(period="1y")
+
+# print(df)
+
+#
+# adx = df.ta.adx()
+
+# rsi = df.ta.rsi()
+# df = pd.concat([df, adx, macd, rsi], axis=1)
+#
+#
+#
+
+
+# # print(rsi)
+# print(df)
---
+++ ta.py
@@ -0,0 +1,15 @@
+import ccxt, yfinance
+import pandas_ta as ta
+import panda as pd
+
+exchange = ccxt.binance()
+
+bars = exchange.fetch_ohlcv('')import ccxt, yfinance
+import pandas_ta as ta
+import panda as pd
+
+exchange = ccxt.binance()
+
+bars = exchange.fetch_ohlcv('VET/USDT', timeframe='5m', limit=500)
+
+print(bars)
